bfa054fd62efd79969fbfd88cdbe3ac68ea34e11
Long Wang <long.w@columbia.edu>
2019-02-03 00:23:10 -0500

    updated extract content script with option -i for hosting ip address

--- extract_content.sh
+++ extract_content.sh
@@ -4,6 +4,23 @@ src_path=$(pwd)
 ros3d_dir="/ros3djs"
 roslib_dir="/roslibjs"
 server_content_root="/var/www/html"
+# read all options
+while [ -n "$1" ]; do # while loop starts
+	case "$1" in
+    	-i) 
+			ip_address="$2"
+			echo "ip adress to host: $ip_address"
+        	shift
+        	;;
+        --)
+        	shift # The double dash makes them parameters
+        	break
+        	;;
+    	*) echo "Option $1 not recognized" ;;
+    esac
+    shift
+done
+
 # copy libraries needed
 mkdir -p "$server_content_root/build" 
 cd "$src_path$ros3d_dir/build"
@@ -22,4 +39,18 @@ cp -r resources "$server_content_root/examples"
 cd "$src_path$roslib_dir/examples"
 echo "Extracting: $(pwd)"
 cp -r user-monitor "$server_content_root/examples"
+# checkout the default files
 
+# replace the default ip address 127.0.0.1
+if [ -z "$ip_address" ] 
+then
+	echo "No dedicated ip address specified, 127.0.0.1 is used by default."
+else
+	echo "$ip_address is used"
+	sed -i "s/127.0.0.1/$ip_address/g" \
+	"$server_content_root/examples/user-monitor/buttons_event.js"
+	sed -i "s/127.0.0.1/$ip_address/g" \
+	"$server_content_root/examples/user-monitor/monitor.html"
+	sed -i "s/127.0.0.1/$ip_address/g" \
+	"$server_content_root/examples/marker_control.html"
+fi
